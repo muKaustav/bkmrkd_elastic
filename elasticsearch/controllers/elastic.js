d9ab69b160a62709edec7889491a9207072fbc46
muKaustav <mu.kaustav@gmail.com>
2023-12-07 05:22:36 +0530

    edit controller

--- elasticsearch/controllers/elastic.js
+++ elasticsearch/controllers/elastic.js
@@ -4,56 +4,103 @@ const { esClient } = require('../elastic')
 let search = async (req, res) => {
     try {
         let searchTerm = req.query.search
+        let page = req.query.page || 1
+        let pageSize = req.query.pageSize || 10
 
-        let query = {
-            "query": {
-                "bool": {
-                    "must": [
-                        {
-                            "multi_match": {
-                                "type": "best_fields",
-                                "query": `${searchTerm}`,
-                                "lenient": true
-                            }
-                        }
-                    ],
-                    "filter": [],
-                    "should": [],
-                    "must_not": []
+        let from = (page - 1) * pageSize
+        let size = pageSize
+
+        if (searchTerm === null || searchTerm === undefined || searchTerm === '') {
+            let query = {
+                "query": {
+                    "match_all": {}
                 }
-            },
-        }
+            }
 
-        let response = await esClient.search({
-            index: process.env.ES_INDEX,
-            body: query
-        })
-
-        if (response.hits.total.value > 0) {
-            let results = response.hits.total.value
-
-            let values = response.hits.hits.map((hit) => {
-                return {
-                    id: hit._id,
-                    title_without_series: hit._source.title_without_series,
-                    book_id: hit._source.book_id,
-                    score: hit._score
-                }
+            let response = await esClient.search({
+                index: process.env.ES_INDEX,
+                body: query,
+                from: from,
+                size: size
             })
 
-            res.status(200).json({
-                data: {
-                    total_results: results,
-                    books: values
-                }
-            })
+            if (response.hits.total.value > 0) {
+                let totalResults = response.hits.total.value
+                let values = response.hits.hits.map((hit) => {
+                    return {
+                        id: hit._id,
+                        title_without_series: hit._source.title_without_series,
+                        book_id: hit._source.book_id,
+                        score: hit._score
+                    }
+                })
+
+                res.status(200).json({
+                    data: {
+                        total_results: totalResults,
+                        books: values
+                    }
+                })
+            } else {
+                res.status(200).json({
+                    data: {
+                        total_results: 0,
+                        books: []
+                    }
+                })
+            }
         } else {
-            res.status(200).json({
-                data: {
-                    total_results: 0,
-                    books: []
+            let query = {
+                "query": {
+                    "bool": {
+                        "must": [
+                            {
+                                "multi_match": {
+                                    "type": "best_fields",
+                                    "query": `${searchTerm}`,
+                                    "lenient": true
+                                }
+                            }
+                        ],
+                        "filter": [],
+                        "should": [],
+                        "must_not": []
+                    }
                 }
+            }
+            
+            let response = await esClient.search({
+                index: process.env.ES_INDEX,
+                body: query,
+                from: from,
+                size: size
             })
+
+            if (response.hits.total.value > 0) {
+                let totalResults = response.hits.total.value
+                let values = response.hits.hits.map((hit) => {
+                    return {
+                        id: hit._id,
+                        title_without_series: hit._source.title_without_series,
+                        book_id: hit._source.book_id,
+                        score: hit._score
+                    }
+                })
+
+                res.status(200).json({
+                    data: {
+                        total_results: totalResults,
+                        books: values
+                    }
+                })
+            } else {
+                res.status(200).json({
+                    data: {
+                        total_results: 0,
+                        books: []
+                    }
+                })
+            }
         }
     } catch (error) {
         console.log(error)
@@ -63,6 +110,7 @@ let search = async (req, res) => {
         })
     }
 }
+
 
 let indexBook = async (req, res) => {
     try {
